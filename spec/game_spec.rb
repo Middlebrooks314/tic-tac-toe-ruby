42a33f889642879af253217f0fa667d8599278c3
Malisa Middlebrooks <Middlebrooks314@gmail.com>
2020-02-17 11:21:21 -0600

    Display board as a string

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,4 +5,14 @@ describe Game, :type => :class do
     it('is valid with valid attributes') do
         expect { Game }.not_to raise_error(NameError)
     end
+
+    it 'prints a 3x3 tic tac toe board' do
+        game = Game.new
+        expected_output  = "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expect{game.display_board}.to output(expected_output).to_stdout
+      end
 end
