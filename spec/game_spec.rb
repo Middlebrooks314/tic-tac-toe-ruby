8553bea3b1cd06f491d465982cec2cc2dbb6432c
Malisa Middlebrooks <Middlebrooks314@gmail.com>
2020-02-19 12:11:52 -0600

    Add full test coverage for displaying_board method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,32 +10,144 @@ describe Game, type: 'class' do
 
     describe "#display_board" do
         it 'prints a blank board when the board array is empty' do
-        game = Game.new
+          board = [" "," "," "," "," "," "," "," "," "]
 
-        board = [" "," "," "," "," "," "," "," "," "]
+        game = Game.new
 
         expected_output  = "   |   |   \n"
         expected_output += "-----------\n"
         expected_output += "   |   |   \n"
         expected_output += "-----------\n"
         expected_output += "   |   |   \n"
-
         expect{game.display_board(board)}.to output(expected_output).to_stdout
       end
 
-      it "prints a board with a character in the center position" do
-      game = Game.new
-      board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+      it "prints a board with an X in the center position" do
+        board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-      expected_output  = "   |   |   \n"
-      expected_output += "-----------\n"
-      expected_output += "   | X |   \n"
-      expected_output += "-----------\n"
-      expected_output += "   |   |   \n"
+        game = Game.new
 
-      expect{game.display_board(board)}.to output(expected_output).to_stdout
-
+        expected_output  = "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   | X |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
     end
 
+      it "prints a board with an O in the corner position" do
+        board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+
+        game = Game.new
+
+        expected_output  = " O |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+    end
+
+      it "prints a board with O winning across the top row" do
+        board = ["O", "O", "O", " ", " ", " ", " ", " ", " "]
+
+        game = Game.new
+
+        expected_output  = " O | O | O \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+    end
+
+      it "prints a board with X winning across the bottom row" do
+        board = [" ", " ", " ", " ", " ", " ", "X", "X", "X"]
+
+        game = Game.new
+
+        expected_output  = "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += " X | X | X \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+    end
+
+      it "prints a board with O winning on the right to left upwards diagonal" do
+        board = [" ", " ", "X", " ", "X", " ", "X", " ", " "]
+
+        game = Game.new
+
+        expected_output  = "   |   | X \n"
+        expected_output += "-----------\n"
+        expected_output += "   | X |   \n"
+        expected_output += "-----------\n"
+        expected_output += " X |   |   \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+    end
+
+      it "prints a board with O winning on the right to left downwards diagonal" do
+        board = ["O", " ", " ", " ", "O", " ", " ", " ", "O"]
+
+        game = Game.new
+
+        expected_output  = " O |   |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   | O |   \n"
+        expected_output += "-----------\n"
+        expected_output += "   |   | O \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+    end
+
+      it "prints a full board with arbitrary X and O values" do
+        board = ["O", "X", "O", "X", "O", "X", "X", "X", "O"]
+
+        game = Game.new
+
+        expected_output  = " O | X | O \n"
+        expected_output += "-----------\n"
+        expected_output += " X | O | X \n"
+        expected_output += "-----------\n"
+        expected_output += " X | X | O \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+
+        board = ["X", "X", "O", "X", "O", "X", "X", "O", "O"]
+
+        game = Game.new
+
+        expected_output  = " X | X | O \n"
+        expected_output += "-----------\n"
+        expected_output += " X | O | X \n"
+        expected_output += "-----------\n"
+        expected_output += " X | O | O \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+      end
+
+      it "prints a full board with X values" do
+        board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+        game = Game.new
+
+        expected_output  = " X | X | X \n"
+        expected_output += "-----------\n"
+        expected_output += " X | X | X \n"
+        expected_output += "-----------\n"
+        expected_output += " X | X | X \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+      end
+
+      it "prints a full board with O values" do
+        board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+        game = Game.new
+
+        expected_output  = " O | O | O \n"
+        expected_output += "-----------\n"
+        expected_output += " O | O | O \n"
+        expected_output += "-----------\n"
+        expected_output += " O | O | O \n"
+        expect{game.display_board(board)}.to output(expected_output).to_stdout
+    end
   end
 end
