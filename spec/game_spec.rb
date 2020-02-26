39f76fc01860003455eb153b836dfb2e4a3121a7
Malisa Middlebrooks <Middlebrooks314@gmail.com>
2020-02-26 13:42:51 -0600

    Remove commented out tests in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -65,49 +65,4 @@ describe Game do
       expect(console.print_message(console.printed_string)).to eq(expected_output)
     end
   end
-  # describe '#input_to_index' do
-  #   it 'will return 4 as the array index when a user inputs 5' do
-  #     game = Game.new
-
-  #     expect(game.input_to_index(5)).to eq(4)
-  #   end
-
-  #   it 'will return 0 as the array index when a user inputs 1' do
-  #     game = Game.new
-
-  #     expect(game.input_to_index(1)).to eq(0)
-  #   end
-  # end
-
-  # describe '#play' do
-
-  #   game = Game.new
-
-  #   it 'receives user input using the gets method' do
-  #     allow($stdout).to receive(:puts)
-  #     expect(game).to receive(:gets).and_return('1')
-
-  #     game.move
-  #   end
-
-  #   it 'calls #input_to_index and #position_taken?' do
-  #     allow($stdout).to receive(:puts)
-  #     expect(game).to receive(:gets).and_return('1')
-  #     expect(game).to receive(:input_to_index).and_return(0)
-  #     expect(game).to receive(:position_taken?).and_return(false)
-
-  #     game.move
-  #   end
-
-  #   it 'takes a user input of 4 and displays a board with an X in the 0th index' do
-  #     allow($stdout).to receive(:puts)
-  #     expect(game).to receive(:gets).and_return('1')
-  #     expect(game).to receive(:input_to_index).and_return(0)
-  #     expect(game).to receive(:display_board)
-
-  #     board = game.instance_variable_get(:@board)
-  #     expect(board).to eq(['X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
-  #   game.move
-    # end
-  # end
 end
