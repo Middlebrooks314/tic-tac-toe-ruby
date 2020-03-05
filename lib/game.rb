b6747ac8920be730be75d0716df63b32f35f9f86
Malisa Middlebrooks <Middlebrooks314@gmail.com>
2020-03-05 12:02:44 -0600

    Add play method, before pairing

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,11 @@ require_relative 'console'
 require_relative 'rules'
 
 class Game
-  def initialize(board, console)
+  def initialize(board, console, rules)
     @board = board
     @console = console
     @current_player = 'X'
+    @rules = rules
   end
 
   def welcome
@@ -16,12 +17,18 @@ class Game
   def turn
     @console.print_message('Please enter a number 1-9')
     index = @console.retrieve_user_input.to_i - 1
-    @board.position_taken?(index) ? play : @board.mark(index, @current_player)
+    @board.position_taken?(index) ? turn : @board.mark(index, @current_player)
     @console.print_message(@board.display)
     switch_players
   end
 
   def switch_players
     @current_player == 'X' ? @current_player = 'O' : @current_player = 'X'
+  end
+
+  def play
+    @console.print_message(@board.display)
+    turn until @rules.game_over?(@board)
+    @console.print_message('Game Over')
   end
 end
